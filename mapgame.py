b8d1880df11dd266ccf3647ee0ac4ec3632d89ba
Bertil Frigaard <bertilfrigaard@gmail.com>
2023-07-17 00:25:06 +0200

    Added guessing guidelines and basic gameflow

--- mapgame.py
+++ mapgame.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 import tkinter
 import tkinter.filedialog as tkfile
 
@@ -19,12 +20,14 @@ class Text:
 
     def draw(self, top, left):
         screen.blit(self.mySurface, (top, left))
-        
 
 class Game:
     def __init__(self, fileData):
         print("Game initiated")
         self.questions = []
+        self.circleStart = [0, (0,0)]
+        self.circleEnd = [0, (0,0)]
+
         for line in fileData:
             self.questions.append(line.split(" "))
 
@@ -32,18 +35,26 @@ class Game:
         self.next()
 
     def next(self):
-        self.nextQuestion = self.questions[0]
+        self.nextQuestion = self.questions.pop(0)
         self.questionText = Text(50, "Find " + self.nextQuestion[0], "black")
     
     def answer(self, pos):
         print("Guess")
+        self.circleStart = [10, pos]
+        self.circleEnd = [10, (int(self.nextQuestion[1]), int(self.nextQuestion[2]))]
+        self.next()
 
     def initBackground(self, img):
         self.image = pygame.transform.scale(img, (1400,800))
 
     def draw(self):
         screen.blit(self.image, (0,0))  
-        self.questionText.draw(50,50)      
+        self.questionText.draw(50,50)   
+        if self.circleStart[0] > 0:
+            self.circleStart[0] -= 1
+            pygame.draw.circle(screen, "red", self.circleStart[1], 15)   
+            pygame.draw.circle(screen, "red", self.circleEnd[1], 15)   
+            pygame.draw.line(screen, "red", self.circleStart[1], self.circleEnd[1], 3)
 
 win = tkinter.Tk
 file = tkfile.askopenfile(initialdir=".")
@@ -53,6 +64,8 @@ game = Game(fileData)
 game.initBackground(pygame.image.load(image))
 
 while running:
+    screen.fill("white")
+    game.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -60,8 +73,6 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             game.answer(pygame.mouse.get_pos())
 
-    screen.fill("white")
-    game.draw()
     pygame.display.flip()
     clock.tick(60)
 
