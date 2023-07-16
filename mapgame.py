f5881846d017eb2e62a4bc263ac956e7d0dbe579
Bertil Frigaard <bertilfrigaard@gmail.com>
2023-07-17 00:01:18 +0200

    Second commit

--- mapgame.py
+++ mapgame.py
@@ -1,11 +1,25 @@
 import pygame
-import time
+import random
 import tkinter
 import tkinter.filedialog as tkfile
+
+pygame.init()
 
 screen = pygame.display.set_mode((1400,800))
 clock = pygame.time.Clock()
 running = True
+
+class Text:
+    def __init__(self, fontSize, text, color):
+        self.update(fontSize, text, color)
+
+    def update(self, fontSize, text, color):
+        self.myfont = pygame.font.SysFont("Roboto", fontSize)
+        self.mySurface = self.myfont.render(text, False, color)
+
+    def draw(self, top, left):
+        screen.blit(self.mySurface, (top, left))
+        
 
 class Game:
     def __init__(self, fileData):
@@ -13,13 +27,23 @@ class Game:
         self.questions = []
         for line in fileData:
             self.questions.append(line.split(" "))
-        print(self.questions)
+
+        random.shuffle(self.questions)
+        self.next()
+
+    def next(self):
+        self.nextQuestion = self.questions[0]
+        self.questionText = Text(50, "Find " + self.nextQuestion[0], "black")
+    
+    def answer(self, pos):
+        print("Guess")
 
     def initBackground(self, img):
         self.image = pygame.transform.scale(img, (1400,800))
 
     def draw(self):
-        screen.blit(self.image, (0,0))        
+        screen.blit(self.image, (0,0))  
+        self.questionText.draw(50,50)      
 
 win = tkinter.Tk
 file = tkfile.askopenfile(initialdir=".")
@@ -32,6 +56,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            game.answer(pygame.mouse.get_pos())
 
     screen.fill("white")
     game.draw()
